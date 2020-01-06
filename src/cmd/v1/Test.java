60cfe8899b83babb5837b86c368e5000c5399059
Sourav Dey <sourav.blackjack@gmail.com>
2020-01-06 20:39:46 +0530

    Unknown chnages

--- src/cmd/v1/Test.java
+++ src/cmd/v1/Test.java
@@ -3,8 +3,8 @@ package Apps.Java.cmd;
 
 public class Test 
 {
-	public static void main(String[] args) 
-	{
-		System.out.println(System.getProperty("user.dir"));
-	}
+    public static void main(String[] args) 
+    {
+        System.out.println(System.getProperty("user.dir"));
+    }
 }
